95082d59c55fc134a0ef21ff9597ac159b84697b
mkratnakumar <43510367+mkratnakumar@users.noreply.github.com>
2021-02-15 00:05:02 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,9 +1,9 @@
 def call(String project) {
 
        
-  sh 'docker build . -t ratnakumar/myapp1:$Docker_tag'
+  sh 'docker build -t ratnakumar/myapp1:v1.0 .
 		   withCredentials([string(credentialsId: 'dockerhub_pass', variable: 'dkr_pass')]) {
      sh 'docker login -u ratnakumar -p $dkr_pass'
-	 sh 'docker push ratnakumar/myapp1:$Docker_tag'
+	 sh 'docker push ratnakumar/myapp1:v1.0'
 }
 }
